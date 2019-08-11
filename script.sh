35253d909fcdd162d10f25cf19332102f7650eca
Ajay Kumar <31384241+ajaysheoran2323@users.noreply.github.com>
2019-08-11 21:53:41 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 CNAME=$(docker ps --format '{{.Names}}')
 if [ ${CNAME}!="vigilant_kilby" ]; then 
-  docker pull ajaysheoran2323/mynewimage:latest && docker run -itd --name vigilant_kilby ajaysheoran2323/mynewimage:latest
+  docker pull ajaysheoran2323/mynewimage:latest && docker run -itd -p 80:80 --name vigilant_kilby ajaysheoran2323/mynewimage:latest
   
 else
-	docker rm -f vigilant_kilby && docker pull ajaysheoran2323/mynewimage:latest && docker run -itd --name vigilant_kilby ajaysheoran2323/mynewimage:latest
+	docker rm -f vigilant_kilby && docker pull ajaysheoran2323/mynewimage:latest && docker run -itd -p 80:80 --name vigilant_kilby ajaysheoran2323/mynewimage:latest
   
 fi
